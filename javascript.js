067d131b0db934143738608543056fe4dc9e1a87
Futureland3 <futureland03@gmail.com>
2025-01-15 13:28:41 +0100

    "Replace individual button functions for one function that works on all Delete comments Disable buttons at the end of the game Finish declareWinner method"

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-const btnRock = document.querySelector("#rock");
-const btnPaper = document.querySelector("#paper");
-const btnScissors = document.querySelector("#scissors");
+
+const btns = document.querySelectorAll("button");
 
 const currentState = document.querySelector(".currentState");
+const currentScores = document.querySelector(".currentScores");
 
 
 let humanChoice = "";
@@ -10,25 +10,19 @@ let computerChoice;
 let roundPlayed = 0
 let scores = [0,0] // score[0] is the player. score[1] is the computer
 
-btnRock.addEventListener("click", ()=>{
-    humanChoice = btnRock.id;
-    playRound(scores);
-});
-btnPaper.addEventListener("click", ()=>{
-    humanChoice = btnPaper.id;
-    playRound(scores);
-
-});
-btnScissors.addEventListener("click", ()=>{
-    humanChoice = btnScissors.id;
-    playRound(scores);
-
+btns.forEach((button) =>{
+    button.addEventListener("click", ()=>{
+        humanChoice = button.id;
+        playRound(scores);
+        if(scores[0] == 5 || scores[1] == 5){  
+            for (const button of btns) {
+                button.disabled = true;
+            }
+            declareWinner(scores);
+        }
+    });
 });
 
-
-function test(){
-    alert("holi");
-}
 
 function getComputerChoice(){
     computerChoice = Math.floor(Math.random() * 3);
@@ -42,76 +36,73 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function declareWinner(finalScores){
-    if(finalScores[0] > finalScores[1]){
-        console.log("You win! Congratulations!");
-    } else if (finalScores[1] > finalScores[0]) {
-        console.log("You lose! Better luck next time!");
+function declareWinner(scores){
+    if(scores[0] > scores[1]){
+        currentState.textContent = "You have won the game! Congratulations! Refresh the page to play again!";
+    } else if (scores[1] > scores[0]) {
+        currentState.textContent = "You have lost the game! Better luck next time! Refresh the page to play again!"
     } else {
-        console.log("It's a tie!");
+        currentState.textContent = "It's a tie at the end! Refresh the page to play again!";
     }
 }
 
-function playRound(scores){
-        console.log("You chose " + humanChoice);
-    
+function playRound(scores){    
         getComputerChoice(3);
-        console.log("Computer chose " + computerChoice);
-
     
         if (humanChoice == "rock" && computerChoice == "paper"){
             scores[1]++;
-            currentState.textContent = "You have lost this round. Computer chose Paper and Paper beats Rock. Current score is: " + scores;
+            currentState.textContent = "You have lost this round. Computer chose Paper and Paper beats Rock.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
 
         }
         else if (humanChoice == "rock" && computerChoice == "rock"){
-            currentState.textContent = "You have both chosen Rock. It's a tie. Current score is: " + scores;
+            currentState.textContent = "You have both chosen Rock. It's a tie.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
 
         }
         else if (humanChoice == "rock" && computerChoice == "scissors"){
             scores[0]++;
-            currentState.textContent = "You have won this round. Computer chose Scissors and Rock beats Scissors. Current score is: " + scores;
+            currentState.textContent = "You have won this round. Computer chose Scissors and Rock beats Scissors.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
 
 
         }
     
         else if (humanChoice == "paper" && computerChoice == "scissors"){
             scores[1]++;
-            currentState.textContent = "You have lost this round. Computer chose Scissors and Scissors beats Paper. Current score is: " + scores;
+            currentState.textContent = "You have lost this round. Computer chose Scissors and Scissors beats Paper.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
         }
         else if (humanChoice == "paper" && computerChoice == "paper"){
-            currentState.textContent = "You have both chosen Paper. It's a tie. Current score is: " + scores;
+            currentState.textContent = "You have both chosen Paper. It's a tie.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
         }
         else if (humanChoice == "paper" && computerChoice == "rock"){
             scores[0]++;
-            currentState.textContent = "You have won this round. Computer chose Rock and Paper beats Rock. Current score is: " + scores;
+            currentState.textContent = "You have won this round. Computer chose Rock and Paper beats Rock.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
         }
     
         else if (humanChoice == "scissors" && computerChoice == "rock"){
             scores[1]++;
-            currentState.textContent = "You have lost this round. Computer chose Rock and Rock beats Scissors. Current score is: " + scores;
+            currentState.textContent = "You have lost this round. Computer chose Rock and Rock beats Scissors.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
         }
         else if (humanChoice == "scissors" && computerChoice == "scissors"){
-            currentState.textContent = "You have both chosen Scissors. It's a tie. Current score is: " + scores;
+            currentState.textContent = "You have both chosen Scissors. It's a tie.";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
         }
         else if (humanChoice == "scissors" && computerChoice == "paper"){
             scores[0]++;
             currentState.textContent = "You have won this round. Computer chose Scissors and Scissors beats Paper";
+            currentScores.textContent = "Player: " + scores[0] + ". Computer: " + scores[1];
+
         }
         else {
             currentState.textContent = "Something unexpected happened. Refresh the page!";
         }
         return scores;
 }
-
-//console.log("You are going to play 5 rounds of Rock Paper Scissors.");
-// if (scores[0] < 5 && scores[1] < 5){
-//     console.log("Round " + (roundPlayed+1));
-//     playRound(scores);
-//     roundPlayed++;
-//     console.log("Your score is now: " + scores[0] + " . And the computer's score is now: " + scores[1]);
-// }
-//declareWinner(scores);
 
 
 
